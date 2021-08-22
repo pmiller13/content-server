26e38cec9911b6b5227aa135aa4e0f0fefbc6e40
p <patrick.mill3r13@gmail.com>
2021-08-21 21:17:03 -0700

    Better tests..?

--- upload_test.py
+++ upload_test.py
@@ -1,15 +1,24 @@
 import requests
 import json
 import base64
-
-with open('/home/p/Downloads/rr.gif', 'rb') as f:
+import pprint
+pp = pprint.PrettyPrinter()
+with open('/home/p/Downloads/test.gif', 'rb') as f:
     data = f.read()
 
 payload = {
     'text': 'quick fox',
     'image': base64.b64encode(data).decode('utf-8')
 }
+upload = "http://0.0.0.0:8080/upload"
+req = requests.post(upload, data=json.dumps(payload))
 
-url = "http://0.0.0.0:8080/upload"
-req = requests.post(url, data=json.dumps(payload))
-print(req.text)
+list_things = "http://0.0.0.0:8080/list"
+req = requests.get(list_things)
+huh = json.loads(requests.get(list_things).text)
+pp.pprint(huh)
+download_thing = "http://0.0.0.0:8080/download"
+req = requests.get(download_thing)
+pp.pprint(json.loads(req.text))
+
+
